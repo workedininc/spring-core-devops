bd0b0efa3e145fab7db474039794afb40f8b73a2
JonathanMabiala <62263731+JonathanMabiala@users.noreply.github.com>
2025-02-17 19:09:52 -0500

    test

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -9,6 +9,6 @@ public class SpringCoreDevOpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
-		//this is a whitespace change te
+		//this is a whitespace change
 	}
 }
